f98a354d560a9455ce93c2cf04980f59bfcad8d2
Brooks <brooksgathagan@gmail.com>
2020-04-11 15:01:27 -0400

    Updated categorization for 2/3-category games in Gameboard.java

--- src/main/java/com/sideprojects/trivialpursuit/model/Category.java
+++ src/main/java/com/sideprojects/trivialpursuit/model/Category.java
@@ -10,4 +10,18 @@ public class Category {
 	
 	public String getCategoryName() { return categoryName;}
 	public void setCategoryName(String categoryName)  { this.categoryName = categoryName;}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if ((obj == null) || (!(obj instanceof Category)))
+			return false;
+		if (super.equals(obj))
+			return true;
+		Category c = (Category) obj;
+		return this.getCategoryId() == c.getCategoryId()
+	}
+	@Override
+	public int hashCode() {
+		return this.getCategoryId().hashCode();
+	}
 }
